0d85c141397d06a58f4bba74eb1db9d9b9a3537a
vivek10c <124761412+vivek10c@users.noreply.github.com>
2025-05-12 19:14:00 +0530

    Update ProjectSection.jsx

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -100,7 +100,7 @@ export const ProjectsSection = () => {
           <a
             className="cosmic-button w-fit flex items-center mx-auto gap-2"
             target="_blank"
-            href="https://github.com/machadop1407"
+            href="https://github.com/vivek10c"
           >
             Check My Github <ArrowRight size={16} />
           </a>
